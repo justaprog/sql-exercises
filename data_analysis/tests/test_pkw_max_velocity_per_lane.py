442b42e5e36bc75b0de93b82a529c633959dc511
Duong <trieuduongdc@gmail.com>
2026-03-03 22:26:06 +0100

    test: add tests for lkw_ratio and lane_2_min_mean_velocity_100_cars in test_pkw_max_velocity_per_lane.py

--- data_analysis/tests/test_pkw_max_velocity_per_lane.py
+++ data_analysis/tests/test_pkw_max_velocity_per_lane.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pipelines.transform import pkw_max_velocity_per_lane
+from pipelines.transform import pkw_max_velocity_per_lane, lkw_ratio, lane_2_min_mean_velocity_100_cars
 import _old_solutions
 
 from isda_streaming.data_stream import KeyedStream, TimedStream
@@ -8,9 +8,25 @@ from isda_streaming.data_stream import KeyedStream, TimedStream
 data_fpath = "data/autobahn.csv"
 
 def test_pkw_max_velocity_per_lane():
-    start = 0
-    end = 30
-    input_stream = TimedStream()
-    input_stream.from_csv(data_fpath, start, end)
+    start = [0, 100, 300]
+    end = [50, 150, 400]
+    for s, e in zip(start, end):
+        input_stream = TimedStream()
+        input_stream.from_csv(data_fpath, s, e)
+        assert pkw_max_velocity_per_lane(input_stream) == _old_solutions.pkw_max_velocity_per_lane(input_stream)
 
-    assert pkw_max_velocity_per_lane(input_stream) == _old_solutions.pkw_max_velocity_per_lane(input_stream)
+def test_pkw_max_velocity_per_lane_empty():
+    start = [0, 100, 300]
+    end = [50, 150, 400]
+    for s, e in zip(start, end):
+        input_stream = TimedStream()
+        input_stream.from_csv(data_fpath, s, e)
+        assert lkw_ratio(input_stream) == _old_solutions.lkw_ratio(input_stream)
+
+def test_lane_2_min_mean_velocity_100_cars():
+    start = [0, 100, 300]
+    end = [50, 150, 400]
+    for s, e in zip(start, end):
+        input_stream = TimedStream()
+        input_stream.from_csv(data_fpath, s, e)
+        assert lane_2_min_mean_velocity_100_cars(input_stream) == _old_solutions.lane_2_min_mean_velocity_100_cars(input_stream)
